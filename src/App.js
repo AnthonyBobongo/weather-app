d6ba17da686bf53600c598f4f0918e9fe774aa87
Anthony Bobongo <anthony.bobongo@gmail.com>
2020-10-04 10:12:09 -0400

    remove items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 const api = {
-  key: '9a0429b772c25562bf00a19bed94eef5',
+  key:
   base: 'https://api.openweathermap.org/data/2.5/'
 }
+require('dotenv').config()
+
+console.log(process.env)
 
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months =["January","February","March","April","May","June","July",
+    "August","September","October","November","December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="app">
       <main>
@@ -14,10 +31,18 @@ function App() {
           type ='text'
           className='search-bar'
           placeholder='Search..'/>
-
+        </div>
+        <div className='location-box'>
+          <div className='location'>New York City, US</div>
+          <div className='date'>{dateBuilder(new Date())}</div>
+        </div>
+        <div className='weather-box'>
+          <div className='temp'>
+            20°C
+          </div>
+          <div className='weather'>Sunny</div>
         </div>
       </main>
-    
     </div>
   );
 }
